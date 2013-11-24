d7422e8110d1a86778741cf2e13ed06828388eb0
klkucan <rickmistgun@163.com>
2013-11-24 23:33:24 +0800

    Revert 82ca5d1..b4d0a08
    This rolls back to commit 82ca5d1ae535ea98325e17fe899528ba26f03df7.

--- PictureMemoryUsageDemo/EntityModels/TripPictureInfo.cs
+++ PictureMemoryUsageDemo/EntityModels/TripPictureInfo.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using System.Windows.Media.Imaging;
-
 namespace PictureMemoryUsageDemo.EntityModels
 {
     public class TripPictureInfo
@@ -13,17 +11,5 @@ namespace PictureMemoryUsageDemo.EntityModels
         public string CityName { get; set; }
         public string StatusCode { get; set; }
         public string TripPictureUrl { get; set; }
-
-        private BitmapImage _tirpPictureSource = new BitmapImage() { CreateOptions = BitmapCreateOptions.BackgroundCreation 
-                                                                                    | BitmapCreateOptions.IgnoreImageCache 
-                                                                                    | BitmapCreateOptions.DelayCreation };
-        public BitmapImage TirpPictureSource
-        {
-            get 
-            {
-                _tirpPictureSource.UriSource = new Uri(TripPictureUrl,UriKind.RelativeOrAbsolute);
-                return _tirpPictureSource;
-            }
-        }
     }
 }
